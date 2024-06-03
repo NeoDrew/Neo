8be61892f649ab8162bfbeb873230f561d7643e9
g52332ar <andrew.robertson-2@student.manchester.ac.uk>
2024-06-03 18:53:50 +0100

    start

---
+++ lexing/lexer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class lexer {
+public:
+    static void run(string file) {
+        // DFA'S INSIDE DFA'S
+        vector<pair<string, string>> line;
+        //make_pair(,)
+        
+
+
+        
+
+    }
+};
+
+int main() {
+
+    lexer::run("int a = 0; int b = a + 7;");
+    return 0;
+}
